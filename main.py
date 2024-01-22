c3ae9fce171c7fd477eef90ba96b846d3a047234
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2024-01-22 22:03:54 +0800

    fix client download

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ apk_version_txt = './APK_VERSION.txt'
 is_dev: bool = file_exists('.idea')
 cur_ver: str = read_text(apk_version_txt).strip()
 op = create_option('op.yml')
-cl: JmHtmlClient = op.new_jm_client()
+html_cl: JmHtmlClient = op.new_jm_client(impl='html')
+api_cl: JmApiClient = op.new_jm_client(impl='api')
 
 
 def add_output(k, v):
@@ -21,14 +22,14 @@ def add_output(k, v):
 
 
 def download_new_ver(new_ver, download_path):
-    resp = cl.get(download_path)
+    resp = html_cl.get(download_path)
     save_resp_content(resp, f'./{new_ver}.apk')
     write_text(apk_version_txt, new_ver)
 
 
 def check_apk():
-    # download_path, new_ver = fetch_apk_ver_and_download_path()
-    download_path, new_ver = fetch_apk_ver_and_download_path2()
+    # new_ver, download_path = fetch_apk_ver_and_download_path()
+    new_ver, download_path = fetch_apk_ver_and_download_path2()
     add_output('new_ver', new_ver)
 
     if new_ver <= cur_ver:
@@ -41,19 +42,18 @@ def check_apk():
 
 
 def fetch_apk_ver_and_download_path():
-    resp = cl.get_jm_html('/stray/?utm_source=18comic')
+    resp = html_cl.get_jm_html('/stray/?utm_source=18comic')
     apk_version_pattern = re.compile(r'a href="(/static/apk/(.*?).apk)"')
     match = PatternTool.require_match(resp.text, apk_version_pattern, '未匹配上apk下载路径', None)
     download_path, new_ver = match[1], match[2]
-    return download_path, new_ver
+    return new_ver, download_path
 
 
 def fetch_apk_ver_and_download_path2():
-    app: JmApiClient = op.new_jm_client(impl=JmApiClient)
-    resp = app.setting()
+    resp = api_cl.setting()
     data = resp.model_data
 
-    return data.download_url, data.version
+    return data.version, data.download_url
 
 
 if __name__ == '__main__':
